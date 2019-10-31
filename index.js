dbd628c50697db9c64d330b07f6ae7f949b9003e
Martins-O-U <martinsonyedikachi@gmail.com>
2019-10-31 16:09:32 +0100

    Edited the sample People's array and deleted the server file

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ server.use(express.json())
 server.use(cors())
 
 
-const friends = [
-  { id: 1, name: 'Blinx', quote: "Nerver give up, there's always a possibility" },
-  { id: 2, name: 'Messi', quote: "Best among the bests footballers"},
-  { id: 3, name: 'Buhari', quote: "One Naira will equal one dollar"},
-];
+const people = [
+    { id: 1, name: 'Blinx', quote: "Nerver give up, there's always a possibility" },
+    { id: 2, name: 'Messi', quote: "Best among the bests footballers"},
+    { id: 3, name: 'Buhari', quote: "One Naira will equal one dollar"},
+  ];
+
 
 server.get('/api/people', (req, res) => {
-  res.json(friends);
+  res.json(people);
 });
 
 server.get('*', (req, res) => {
